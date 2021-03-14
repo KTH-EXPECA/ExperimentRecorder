1f0d41faa250db2a24668c95930fd73838a85338
Manuel Olguín Muñoz <manuel@olguin.se>
2021-03-14 11:38:22 -0400

    parallel thread for db io

--- exprec/db_session.py
+++ exprec/db_session.py
@@ -12,15 +12,16 @@
 #  See the License for the specific language governing permissions and
 #  limitations under the License.
 import threading
+import time
 from collections import deque
-from queue import Empty, Queue
-from typing import Any, Callable, Collection, Mapping, Optional
+from dataclasses import dataclass, field
+from queue import Empty, PriorityQueue, Queue
+from typing import Any, Callable, Collection, Iterable, Mapping, Optional
 
 from contextlib2 import contextmanager
 from sqlalchemy.engine import create_engine
 from sqlalchemy.orm import Session, scoped_session, sessionmaker
 
-# noinspection PyPep8Naming
 from .models import Base
 
 
@@ -67,14 +68,27 @@ class DBInstCreationPromise:
 
     def get_result(self, timeout: Optional[float] = None) -> Mapping[str, Any]:
         with self._cond:
-            while not self._has_result:
+            if not self._has_result:
                 self._cond.wait(timeout=timeout)
+                if not self._has_result:
+                    # timeout
+                    raise TimeoutError('Timed out waiting for Promise result.')
+                else:
+                    return self._result
 
-        return self._result
+
+@dataclass(frozen=True)
+class _PrioQueueElement:
+    priority: int
+    timestamp: float
+    promise: DBInstCreationPromise = field(compare=False)
 
 
 # noinspection PyPep8Naming
 class ExperimentWriterThread(threading.Thread):
+    urgent_prio = 0
+    relaxed_prio = 100
+
     def __init__(self,
                  db_address: str = 'sqlite:///:memory:',
                  buf_size: int = 100):
@@ -82,40 +96,41 @@ class ExperimentWriterThread(threading.Thread):
         self._db_address = db_address
         self._buf_size = buf_size
 
-        self._urgent_ops: Queue[DBInstCreationPromise] = Queue()
-        self._operations: Queue[DBInstCreationPromise] = Queue()
+        # use a priority queue to always yield urgent instances first
+        self._instances: Queue[_PrioQueueElement] = PriorityQueue()
 
         self._shutdown = threading.Event()
         self._shutdown.clear()
 
-        self._flush_lock = threading.RLock()
+        self._lock = threading.Lock()
         self._t_exception = None
 
-    def eager_create(self,
-                     fn: Callable[[Session], Base],
-                     return_attributes: Collection[str]) -> Mapping[str, Any]:
-        with self._flush_lock:
-            if self._shutdown.is_set():
-                raise RuntimeError('DB session is shut down.')
+    def urgent_create(self,
+                      fn: Callable[[Session], Base],
+                      return_attributes: Collection[str],
+                      timeout: float = 0.1) -> Mapping[str, Any]:
+        promise = DBInstCreationPromise(fn, return_attributes)
+        self._instances.put(_PrioQueueElement(
+            priority=self.urgent_prio,
+            timestamp=time.monotonic(),
+            promise=promise
+        ))
+        return promise.get_result(timeout=timeout)
 
-            promise = DBInstCreationPromise(fn, return_attributes)
-            self._urgent_ops.put(promise)
-            return promise.get_result()
-
-    def deferred_execute(self,
-                         fn: Callable[[Session], Base],
-                         return_attributes: Collection[str]) \
+    def relaxed_create(self,
+                       fn: Callable[[Session], Base],
+                       return_attributes: Collection[str]) \
             -> DBInstCreationPromise:
-        with self._flush_lock:
-            if self._shutdown.is_set():
-                raise RuntimeError('DB session is shut down.')
-
-            promise = DBInstCreationPromise(fn, return_attributes)
-            self._operations.put(promise)
-            return promise
+        promise = DBInstCreationPromise(fn, return_attributes)
+        self._instances.put(_PrioQueueElement(
+            priority=self.relaxed_prio,
+            timestamp=time.monotonic(),
+            promise=promise
+        ))
+        return promise
 
     def sanity_check(self) -> None:
-        with self._flush_lock:
+        with self._lock:
             if self._t_exception is not None:
                 raise self._t_exception
 
@@ -124,84 +139,56 @@ class ExperimentWriterThread(threading.Thread):
         self.join()
         self.sanity_check()
 
-    # noinspection PyProtectedMember
     def run(self) -> None:
         # parallel execution thread.
         engine = create_engine(self._db_address)
         scoped = scoped_session(sessionmaker(bind=engine))
         op_buf = deque(maxlen=self._buf_size)
+
+        # noinspection PyProtectedMember
+        def _add_commit_all(promises: Iterable[DBInstCreationPromise]) \
+                -> None:
+            with session_context(scoped) as sess:
+                instances = [p._make_instance(sess) for p in promises]
+                sess.add_all(instances)
+                sess.commit()
+
+                for promise, instance in zip(promises, instances):
+                    promise._set_ret_attr_from_instance(instance)
+
         try:
             while not self._shutdown.is_set():
                 try:
-                    urgent_op = self._urgent_ops.get(timeout=0.01)
-                    # if we get an urgent op, execute it immediately
-                    with session_context(scoped) as session:
-                        inst = urgent_op._make_instance(session)
-                        session.add(inst)
-                        session.commit()
-
-                        # register the results
-                        urgent_op._set_ret_attr_from_instance(inst)
+                    q_elem = self._instances.get(timeout=0.1)
                 except Empty:
-                    # if no urgent ops, check if deferred ops
-                    rem_buf_space = op_buf.maxlen - len(op_buf)
-                    for i in range(rem_buf_space):
-                        try:
-                            op_buf.append(self._operations.get_nowait())
-                        except Empty:
-                            break
+                    continue
 
+                if q_elem.priority == self.urgent_prio:
+                    # execute immediately
+                    _add_commit_all([q_elem.promise])
+                else:
+                    # if not urgent, put into buffer, then check buffer
+                    # size to decide if it's time to execute
+                    op_buf.append(q_elem.promise)
                     if len(op_buf) >= op_buf.maxlen:
                         # flush op_buf to db
-                        with session_context(scoped) as session:
-                            instances = [op._make_instance(session)
-                                         for op in op_buf]
-                            session.add_all(instances)
-                            session.commit()
-
-                            for promise, inst in zip(op_buf, instances):
-                                promise._set_ret_attr_from_instance(inst)
+                        _add_commit_all(op_buf)
                         op_buf.clear()
 
-            with self._flush_lock:
-                self._shutdown.set()
-                # flush whatever is left
-                try:
-                    # start with urgent ops
-                    while True:
-                        urgent_op = self._urgent_ops.get_nowait()
-                        # if we get an urgent op, execute it immediately
-                        with session_context(scoped) as session:
-                            inst = urgent_op._make_instance(session)
-                            session.add(inst)
-                            session.commit()
+            # flush whatever is left
+            rem = deque(op_buf)
+            op_buf.clear()
+            try:
+                while True:
+                    rem.append(self._instances.get_nowait())
+            except Empty:
+                pass
 
-                            # register the results
-                            urgent_op._set_ret_attr_from_instance(inst)
-                except Empty:
-                    pass
+            _add_commit_all(rem)
+            rem.clear()
 
-                # next, non-urgent
-                # first, deque then directly from queue
-                with session_context(scoped) as session:
-                    promises = [op for op in op_buf]
-                    instances = [op._make_instance(session) for op in promises]
-                    op_buf.clear()
-
-                    try:
-                        promise = self._operations.get_nowait()
-                        promises.append(promise)
-                        instances.append(promise._make_instance(session))
-                    except Empty:
-                        pass
-
-                    session.add_all(instances)
-                    session.commit()
-
-                    for promise, inst in zip(promises, instances):
-                        promise._set_ret_attr_from_instance(inst)
         except Exception as e:
-            with self._flush_lock:
-                self._shutdown.set()
+            self._shutdown.set()
+            with self._lock:
                 self._t_exception = e
                 raise e
