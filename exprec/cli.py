cd6bfd1147d87029806ee2c58a3964203247ac5b
Manuel Olguín Muñoz <manuel@olguin.se>
2021-03-15 10:45:14 -0400

    cli testing

--- exprec/cli.py
+++ exprec/cli.py
@@ -11,13 +11,77 @@
 #  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 #  See the License for the specific language governing permissions and
 #  limitations under the License.
-from typing import TextIO
+from pathlib import Path
+from typing import Any, Collection, Mapping, TextIO
 
 import click
+import pandas as pd
 # TODO add logging
 import toml
+from sqlalchemy import create_engine
+from sqlalchemy.orm import Session
+from sqlalchemy.pool import StaticPool
+from twisted.internet import reactor
+from twisted.internet.endpoints import TCP4ServerEndpoint, TCP6ServerEndpoint, \
+    UNIXServerEndpoint
+from twisted.internet.posixbase import PosixReactorBase
 
 from .config import validate_config
+from .exp_interface import BufferedExperimentInterface
+from .models import *
+from .protocol import MessageProtoFactory
+
+reactor: PosixReactorBase = reactor
+
+
+def _records_to_dataframe(db_session: Session,
+                          exp_ids: Collection[uuid.UUID]) -> pd.DataFrame:
+    # collects all variable records into a table
+    # Base.metadata.create_all(engine)
+    # session_fact = sessionmaker(bind=engine)
+    # session = session_fact()
+
+    # set up tables
+    query = db_session.query(
+        ExperimentInstance.id.label('experiment'),
+        InstanceVariable.name.label('variable'),
+        VariableRecord.timestamp.label('timestamp'),
+        VariableRecord.value.label('value')
+    ).filter(ExperimentInstance.id.in_(exp_ids)) \
+        .filter(InstanceVariable.instance_id == ExperimentInstance.id) \
+        .filter(VariableRecord.variable_id == InstanceVariable.id) \
+        .all()
+
+    df = pd.DataFrame([r._asdict() for r in query])
+    # session.close()
+    df = df.pivot(index=['experiment', 'timestamp'],
+                  columns='variable',
+                  values='value')
+    return df
+
+
+def _metadata_to_json(session: Session,
+                      exp_ids: Collection[uuid.UUID]) \
+        -> Mapping[uuid.UUID, Any]:
+    # returns the experiment metadata in the DB as a json-compliant dict
+    output = {}
+    for exp_id in exp_ids:
+        query = session \
+            .query(ExperimentInstance.id,
+                   ExperimentMetadata.label,
+                   ExperimentMetadata.value) \
+            .filter(ExperimentInstance.id == exp_id) \
+            .filter(ExperimentInstance.id == ExperimentMetadata.instance_id) \
+            .all()
+
+        output[exp_id] = {q.label: q.value for q in query}
+    return output
+
+
+def _aggregate_and_output(engine: Engine,
+                          exp_ids: Collection[uuid.UUID],
+                          output_path: Path):
+    pass
 
 
 @click.command()
@@ -31,3 +95,48 @@ def main(config_file: TextIO) -> None:
     config = toml.load(config_file)
     config = validate_config(config)
 
+    # initialize engine
+    engine = create_engine(f'sqlite://{config["database"]["path"]}',
+                           connect_args={'check_same_thread': False},
+                           poolclass=StaticPool)
+    interface = BufferedExperimentInterface(db_engine=engine)
+    protocol_fact = MessageProtoFactory(interface)
+
+    # ready to listen on whatever the config says
+    sock_cfg = config['socket']
+    if sock_cfg['type'] == 'unix':
+        # listen on a unix socket
+        endpoint = UNIXServerEndpoint(reactor=reactor,
+                                      address=sock_cfg['path'],
+                                      backlog=sock_cfg['backlog'])
+    elif sock_cfg['type'] == 'tcp4':
+        endpoint = TCP4ServerEndpoint(reactor=reactor,
+                                      interface=sock_cfg['interface'],
+                                      port=sock_cfg['port'],
+                                      backlog=sock_cfg['backlog'])
+    elif sock_cfg['type'] == 'tcp6':
+        endpoint = TCP6ServerEndpoint(reactor=reactor,
+                                      interface=sock_cfg['interface'],
+                                      port=sock_cfg['port'],
+                                      backlog=sock_cfg['backlog'])
+    else:
+        # should never happen
+        raise RuntimeError(f'Invalid socket type {sock_cfg["type"]}.')
+
+    endpoint.listen(protocol_fact)
+
+    def _shutdown():
+        # on shutdown, we close the interface and write out to the CSV file
+        interface.close()
+        _aggregate_and_output(engine=engine,
+                              exp_ids=interface.experiment_instances,
+                              output_path=config['output']['directory'])
+
+        # finally, delete the db file if needed
+        if not config['database']['persist']:
+            config['database']['path'].unlink(missing_ok=True)
+
+    # clean shutdown
+    reactor.addSystemEventTrigger('before', 'shutdown', _shutdown)
+
+    reactor.run()
---
+++ tests/test_cli.py
@@ -0,0 +1,102 @@
+#  Copyright (c) 2021 KTH Royal Institute of Technology
+#
+#  Licensed under the Apache License, Version 2.0 (the "License");
+#  you may not use this file except in compliance with the License.
+#  You may obtain a copy of the License at
+#
+#         http://www.apache.org/licenses/LICENSE-2.0
+#
+#  Unless required by applicable law or agreed to in writing, software
+#  distributed under the License is distributed on an "AS IS" BASIS,
+#  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#  See the License for the specific language governing permissions and
+#  limitations under the License.
+import datetime
+
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy.pool import StaticPool
+from twisted.trial import unittest
+
+from exprec.cli import _metadata_to_json, _records_to_dataframe
+from exprec.exp_interface import BufferedExperimentInterface
+from exprec.models import Base
+
+dummy_data = {
+    'experiment1': {
+        'metadata': {'name': 'experiment_one'},
+        'records' : {
+            datetime.datetime.fromtimestamp(1): {'a': 1, 'b': 2, 'c': 3},
+            datetime.datetime.fromtimestamp(2): {'a': 2, 'b': 3, 'c': 4},
+            datetime.datetime.fromtimestamp(3): {'a': 3, 'b': 4, 'c': 5}
+        },
+    },
+    'experiment2': {
+        'metadata': {'name': 'experiment_two'},
+        'records' : {
+            datetime.datetime.fromtimestamp(1): {'a': 1, 'b': 2, 'c': 3},
+            datetime.datetime.fromtimestamp(2): {'a': 2, 'b': 3, 'c': 4},
+            datetime.datetime.fromtimestamp(3): {'a': 3, 'b': 4, 'c': 5},
+            datetime.datetime.fromtimestamp(4): {'a': 4, 'b': 5, 'c': 6}
+        },
+    }
+}
+
+
+class TestCLI(unittest.TestCase):
+    def setUp(self) -> None:
+        self._buf_size = 10
+        self._engine = create_engine(
+            'sqlite:///:memory:',
+            connect_args={'check_same_thread': False},
+            poolclass=StaticPool)
+        Base.metadata.create_all(self._engine)
+        self._session_fact = sessionmaker(bind=self._engine)
+        self._session = self._session_fact()
+
+        interface = BufferedExperimentInterface(buf_size=self._buf_size,
+                                                db_engine=self._engine)
+
+        # fill with dummy data
+        self.experiments = {}
+        self._expected_rows = 0
+        for exp_name, exp_data in dummy_data.items():
+            exp_id = interface.new_experiment_instance()
+            self.experiments[exp_name] = exp_id
+
+            interface.add_metadata(exp_id, **exp_data['metadata'])
+            for timestamp, variables in exp_data['records'].items():
+                interface.record_variables(
+                    experiment_id=exp_id,
+                    timestamp=timestamp,
+                    **variables
+                )
+                self._expected_rows += 1
+
+        # flush, to make sure everything is in the database
+        interface.flush()
+        interface.close()
+
+    def tearDown(self) -> None:
+        self._session.close()
+
+    def test_db_to_dataframe(self) -> None:
+        df = _records_to_dataframe(self._session,
+                                   exp_ids=self.experiments.values())
+        self.assertEqual(df.shape[0], self._expected_rows)
+        print('DataFrame:')
+        print(df)
+
+    def test_metadata_dict(self) -> None:
+        metadata = _metadata_to_json(self._session,
+                                     exp_ids=self.experiments.values())
+        print('Metadata:')
+        print(metadata)
+
+        for exp_name, exp_id in self.experiments.items():
+            expected_metadata = dummy_data[exp_name]['metadata']
+            mdata = metadata[exp_id]
+
+            for k, v in expected_metadata.items():
+                self.assertIn(k, mdata)
+                self.assertEqual(mdata[k], v)
